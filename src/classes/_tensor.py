2f43f6efe55814fcea0e0b286e1db28c6701511f
felagorn <69029805+felagorn@users.noreply.github.com>
2022-10-30 20:06:17 +0700

    fix: Tensor backpropagation algorithm
    src.classes.Tensor
    - fix: backward() calls will work correctly now
    - delete: remove zero_grad() and step() methods, since these will be implemented in a separate optim subpackage

--- src/classes/_tensor.py
+++ src/classes/_tensor.py
@@ -1,4 +1,5 @@
-from typing import Callable
+from collections.abc import Callable
+from typing_extensions import Self
 from cupy import ndarray as cdarray, zeros
 from numpy import ndarray
 
@@ -6,7 +7,7 @@ from ..functions import type_check
 
 class Tensor:
 
-    def __init__(self, nd: ndarray, parents: list = [], grad_fn: Callable = None, split_idx: int = -1) -> None:
+    def __init__(self, nd: ndarray, parents: list[Self] = [], grad_fn: Callable[[Self], None] = None, split_idx: int = -1) -> None:
 
         # TYPE CHECKS
         type_check(nd, "nd", cdarray)
@@ -18,36 +19,31 @@ class Tensor:
         self.grad_fn: Callable = grad_fn
         self.grad: ndarray = zeros(nd.shape)
         self.split_idx: int = split_idx
-        self.backward_countdown: int = 0
+        self.i_backward: int = 0
+        self.n_backward: int = 0
         for parent in parents:
-            parent.backward_countdown += 1
+            parent.n_backward += 1
+    
+    def _zero_i_backward(self) -> None:
+        if self.i_backward != 0:
+            self.i_backward = 0
+            if self.grad_fn is not None:
+                for parent in self.parents:
+                    parent._zero_i_backward()
 
-    def _backward(self):
-        self.backward_countdown -= 1
-        if self.backward_countdown < 0:
-            raise AssertionError(f"Backpropagation error found: too many backward calls to a tensor (backward_countdown={self.backward_countdown}).")
-        elif (self.backward_countdown == 0) and (self.grad_fn is not None):
+    def _backward(self) -> None:
+        self.i_backward += 1
+        if self.i_backward > self.n_backward:
+            raise AssertionError(f"Backpropagation error found: too many backward calls to a tensor (i_backward={self.i_backward}, n_backward={self.n_backward}).")
+        elif (self.i_backward == self.n_backward) and (self.grad_fn is not None):
             self.grad_fn(self)
             for parent in self.parents:
                 parent._backward()
-    
-    def _backward_check(self):
-        if self.backward_countdown != 0:
-            raise AssertionError(f"Backpropagation error found: unexpected number of backward calls to a tensor (backward_countdown={self.backward_countdown})")
 
-    def backward(self):
+    def backward(self) -> None:
+        self._zero_i_backward()
+        _n_backward: int = self.n_backward
+        self.n_backward = 1
         self.grad += 1
         self._backward()
-        self._backward_check()
-
-    def zero_grad(self):
-        self.grad = zeros(self.nd.shape)
-        if self.grad_fn is not None:
-            for parent in self.parents:
-                parent.zero_grad()
-
-    def step(self,lr: float):
-        self.nd = self.nd - (self.grad * lr)
-        if self.grad_fn is not None:
-            for parent in self.parents:
-                parent.step(lr)
+        self.n_backward = _n_backward
