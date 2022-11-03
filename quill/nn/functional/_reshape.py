9e09d57dc667fb51bb0172189cb94ac7d2f164d2
felagorn <69029805+felagorn@users.noreply.github.com>
2022-11-03 18:29:14 +0700

    feat: add reshape functional
    quill.nn.functional.reshape
    - feat: wrapper for cupy.reshape
    
    Co-Authored-By: Jonathan Richard Sugandhi <64012903+jonathanrichard13@users.noreply.github.com>

---
+++ quill/nn/functional/_reshape.py
@@ -0,0 +1,15 @@
+from ...classes import Tensor
+from ...functions import type_check
+
+def reshape(x: Tensor, newshape: tuple[int]) -> Tensor:
+
+    # TYPE CHECKS
+    # x must be a Tensor
+    # newshape must be a tuple of ints
+    type_check(x, "x", Tensor)
+    type_check(newshape, "newshape", tuple, int)
+
+    def grad_fn(child: Tensor) -> None:
+        x.grad += child.grad.reshape(x.grad.shape)
+
+    return Tensor(x.nd.reshape(newshape), [x], grad_fn=grad_fn)
